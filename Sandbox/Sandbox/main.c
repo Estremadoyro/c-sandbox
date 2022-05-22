62337498d21a9b9c885eb4b7027ed7de0a358cea
Leonardo <lmestremadoyro@gmail.com>
2022-05-22 01:24:16 -0500

    first sandbox

---
+++ Sandbox/Sandbox/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Sandbox
+//
+//  Created by Leonardo  on 22/05/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  printNumber(120);
+  return 0;
+}
+
+void printNumber(const int number) {
+  printf("Number: ", number);
+  return nil;
+}
---
+++ new-sandbox.c
@@ -0,0 +1,14 @@
+// Imports
+#include <stdio.h>
+
+void wof(const int number) { 
+    printf("Number sent: %o \n", number);
+}
+
+// main()
+int main(int argc, const char * argv[]) { 
+    printf("Hello my friends\n");
+    wof(200);
+    return 0;
+}
+
